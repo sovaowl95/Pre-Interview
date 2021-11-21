0d06d90e1c25685606a6da53082e00dc02990151
VoroninMaxim <x-men-max@yandex.ru>
2021-11-21 20:58:54 +0400

    init

---
+++ src/main/java/Solution.java
@@ -0,0 +1,47 @@
+public class Solution {
+  /**
+   * Дан массив чисел. Найдите 2 числа, сумма которых равна target.
+   * Верните индексы этих чисел
+   * <p>
+   * Пример:
+   * nums = new int{2, 7, 11, 15};
+   * target = 9
+   * result = {0, 1}
+   * <p>
+   * nums = new int{3,2,4};
+   * target = 6
+   * result = {1, 2}
+   * <p>
+   * nums = new int{3,3};
+   * target = 6
+   * result = {0, 1}
+   *
+   * @param nums   массив с числами
+   * @param target то, чему должна быть равна сумма двух чисел
+   * @return массив из двух индексов
+   */
+  public int[] twoSum(int[] nums, int target) {
+    //todo: write your code here
+    return new int[0];
+  }
+
+
+  /**
+   * На вход даны два числа. Длина и Ширина будущего массива.
+   * Заполните массив по спирали начиная с координат[0][0]
+   * Пример:
+   * length = 3, width = 3
+   * Результат:
+   * 1 2 3
+   * 8 9 4
+   * 7 6 5
+   *
+   * @param length длина получившегося массива
+   * @param width ширина получившегося массива
+   * @return заполненный массив
+   */
+  public int[][] fill(int length, int width) {
+    //todo: write your code here
+    return new int[0][0];
+  }
+}
